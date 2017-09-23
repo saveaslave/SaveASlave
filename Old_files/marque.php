cf80d085d89102a5389259b77f5c6e11b3fe3e1c
Francisco Garcia <fgarcia.student04@gmail.com>
2017-09-23 18:49:03 -0400

    removing unneccessary files

--- Old_files/marque.php
+++
@@ -1,8 +0,0 @@
- <div id="marque">
-    
-  <script src="jquery-1.3.2.min.js" type="text/javascript"></script>
-  <script src="jquery.marquee.js" type="text/javascript"></script>
-
-  <marquee behavior="scroll" direction="left" scrollamount="2" width="900">
-   <p><h3>Please promote this web site in any legal, moral, and ethical way that you can.</h3></p></marquee>    
-  </div> 
--- Old_files/navigation.php
+++
@@ -1,9 +0,0 @@
-  <div>
-    <div id="nav"> 
-    	<ul>
-        	<li><a href="index.html">Home</a></li>
-        	<li><a href="whoweare.html">Who We Are</a></li>
-        	<li><a href="paypalbutton.html">Gifting</a></li>
-        </ul>
-    </div>
-   </div>
