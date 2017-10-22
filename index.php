72cff859cfa4e89265dbddef810ad46063f0de68
susendeep <susendeep.d@somaiya.edu>
2017-10-22 21:41:50 +0530

    moving files into old folder

--- index.php
+++ index.php
@@ -1,157 +1,55 @@
-<?php include 'header-home.php'; ?>
-
-<div class="col-md-8">
-<div class="wrap">
-					
-<span class="text-top">Information and education about modern day slavery.
-
-Millions of men, women, and even children are forced into debt bondage, bonded labor, attached labor, restack, forced labor, indentured servitude, and human trafficking each year. They used for work or sex without pay with threats and even brain washed into loving their pimps today.
-
-<br/>You are somebody that can do something to help.
-
-<br/>Send content; forward this. Tell somebody. This website was incorporated in Texas and needs the help of free people just like you from all over the world.</span>   
- <hr class="style13">	
-<?php $_GET['page'] = "index.html";
-@include($_SERVER['DOCUMENT_ROOT'] . "/listlink.php");  ?>
-<hr class="style11">
-
-
-<!--
-<div class="media">
-<a class="pull-left" href="#">
-<img class="media-object" src="photos/Cast-Logo-Print.png" alt="">
-</a>
-<div class="media-body">
-
-<h3>Coalition Members | CAST</h3>
-                        <p>CAST is, by its name, a coalition of organizations who share a commitment to human rights and social justice by ending the modern-day slave trade.</p>    <span class="label label-danger"><a href="coalition-members-cast.php">Read More</a></span></div>
-                </div>		 
- <hr class="style11">	
-
- <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="photos/article8a.jpg" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h3>U.S. Upgrades Uzbekistan, Demotes Belarus In Human Trafficking Report</h3>
-                        <p>The United States has upgraded Uzbekistan and lowered Belarus in its annual human-trafficking report, which ranks 188 countries on their efforts in combating the sex trade and other exploitative labor.
-
-</p>    <span class="label label-danger"><a href="news7.php">Read More</a></span></div>
-                </div>		    
-		
-		 <hr class="style11">
-
- <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="photos/article7.jpg" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h3>Schindler Rescuing ISIS Sex 
-Slaves</h3>
-                        <p>The Islamic State has kidnapped thousands of 
-Yazidi and Christian girls and sold them as sex 
-slaves for Islamic militants.
-Now one Jewish man from Canada is on a mission 
-to rescue as many girls as he can.</p>    <span class="label label-danger"><a href="news1.php">Read More</a></span></div>
-                </div>		 
- <hr class="style11">	
- <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="photos/article1.jpg" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h3>Modern Slavery, a different kind of slavery without locked doors</h3>
-                        <p>Slavery is often associated only with forced 
-labour and the African American movement in the 
-USA. But what people tend to ignore is that there 
-are still different kinds of modern-day slavery, 
-which vary from human trafficking, to bonded 
-labour, domestic and child slavery. </p>    <span class="label label-danger"><a href="news2.php">Read More</a></span></div>
-                </div>		 
- <hr class="style11">	
- <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="photos/article2.jpg" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h3>Dealing with Slavery and Human Trafficking in 
-Belarus</h3>
-                        <p>On 6 February a Belarusian businessman received 
-5 years in prison for enslaving a group of 
-Vietnamese whom he had earlier agreed to deliver 
-to the European Union. Meanwhile, the Belarusian 
-government has defined fighting human trafficking 
-as one of its priorities both domestically and 
-internationally, where it feels it has been successful.</p>    <span class="label label-danger"><a href="news3.php">Read More</a></span></div>
-                </div>		 	
-
- <hr class="style11">	
- <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="photos/article3a.jpg" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h3>Hundreds of young people trafficked into 
-door-to-door sales in the US</h3>
-                        <p>Hundreds of young people are being abused and exploited within the travelling sales industry in the US.
-
-In a report published on Thursday, entitled Knocking at Your Door, the anti-trafficking charity Polaris describes how unemployed 
-young people are targeted by recruiters who promise them an enjoyable job involving travel and high profits.
-
-</p>    <span class="label label-danger"><a href="news4.php">Read More</a></span></div>
-                </div>		
-								
- <hr class="style11">	
- <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="photos/article4.png" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h3>Ending Slavery is Not an Impossible Dream</h3>
-                        <p>
- <span class="small">C Haahr, Human Trafficking Task Force</span> gear up 
-to fight modern-day slavery in Missouri
-
-Information & Education
-
-About Modern Day Slavery
-JEFFERSON CITY, Mo. - Rep. Elijah Haahr, R-
-Springfield, will chair a committee made up of more 
-than 20 individuals from law enforcement, political, 
-child well-being, and public safety groups designed 
-to stop human trafficking in the state of Missouri.
-
-                                            </p>    <span class="label label-danger"><a href="news5.php">Read More</a></span></div>
-                </div>
-								
-<hr class="style11">	
- <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="photos/article5.jpg" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h3>Haahr, Human Trafficking Task Force gear up to fight modern-day slavery in Missouri</h3>
-                        <p>
- <span class="small">JEFFERSON CITY, Mo. - Rep. Elijah Haahr, R-
-Springfield </span>, will chair a committee made up of more 
-than 20 individuals from law enforcement, political, 
-child well-being, and public safety groups designed 
-to stop human trafficking in the state of Missouri.
-                                            </p>    <span class="label label-danger"><a href="news6.php">Read More</a></span></div>
-                </div>	
-
- <hr class="style13">	
--->
-<?php 
-$_GET['page'] = "index.html";
-@include($_SERVER['DOCUMENT_ROOT'] . "/articlelist.php");
-//@include($_SERVER['DOCUMENT_ROOT'] . "/addarticle.php");  
-?>
-<h2><?php echo $add_an_article; ?></h2>
-<iframe src="addarticle.php" width="725" height="950" scrolling="no" align="top"></iframe>
-
-</div>     
-</div>
-
-<?php include 'sidebar-home.php'; ?>	 
+<?php include 'header-home.php'; ?>
+
+
+
+<div class="col-md-8">
+
+<div class="wrap">
+
+					
+
+<span class="text-top">Information and education about modern day slavery.
+
+
+
+Millions of men, women, and even children are forced into debt bondage, bonded labor, attached labor, restack, forced labor, indentured servitude, and human trafficking each year. They used for work or sex without pay with threats and even brain washed into loving their pimps today.
+
+
+
+<br/>You are somebody that can do something to help.
+
+
+
+<br/>Send content; forward this. Tell somebody. This website was incorporated in Texas and needs the help of free people just like you from all over the world.</span>   
+
+ <hr class="style13">	
+
+<?php $_GET['page'] = "index.html";
+
+@include($_SERVER['DOCUMENT_ROOT'] . "/listlink.php");  ?>
+
+<hr class="style11">
+
+<?php 
+
+$_GET['page'] = "index.html";
+
+@include($_SERVER['DOCUMENT_ROOT'] . "/articlelist.php"); 
+
+?>
+
+<h2><?php echo $add_an_article; ?></h2>
+
+<iframe src="addarticle.php" width="725" height="950" scrolling="no" align="top"></iframe>
+
+
+
+</div>     
+
+</div>
+
+
+
+<?php include 'sidebar-home.php'; ?>	 
+
 <?php include 'footer.php'; ?>
